bcf0fa3a67fc84a34290725e5ebc077356b1bd60
leemhoon00 <leemhoon000@gmail.com>
2024-07-01 02:35:53 +0900

    build: Root 폴더의 .env 사용

--- src/module/environment/environment.module.ts
+++ src/module/environment/environment.module.ts
@@ -1,14 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { getEnvFilePath, validate } from './environment';
+import { validate } from './environment';
 import { EnvironmentService } from './environment.service';
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [getEnvFilePath()],
       isGlobal: true,
       cache: true,
       validate,
