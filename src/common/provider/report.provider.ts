6148d87c3205a981bb8c270b5c733a3a12beb9cf
piper-hyowon <hyowon09877@gmail.com>
2024-08-07 20:22:51 +0900

    refactor: 디스코드 에러 리포팅 컬러 추가

--- src/common/provider/report.provider.ts
+++ src/common/provider/report.provider.ts
@@ -12,7 +12,7 @@ export class ReportProvider {
     this.errorReport('error', error, extra, moduleName);
   }
 
-  static report(
+  static info(
     message: string,
     extra?: Record<string, unknown>,
     moduleName?: string,
@@ -21,6 +21,13 @@ export class ReportProvider {
       message,
       extra: { ...extra, moduleName },
       level: 'info',
+    });
+
+    this.sendToDiscord({
+      severity: 'info',
+      title: message,
+      extra,
+      moduleName,
     });
   }
 
@@ -53,11 +60,43 @@ export class ReportProvider {
     });
 
     // Send error to Discord
+    this.sendToDiscord({
+      severity,
+      title: error.message,
+      error,
+      extra,
+      moduleName,
+    });
+  }
+
+  private static sendToDiscord(params: {
+    severity: SeverityLevel;
+    title: string;
+    error?: Error;
+    extra?: Record<string, unknown>;
+    moduleName?: string;
+  }): void {
+    const { severity, title, error, extra, moduleName } = params;
+    let color: number;
+    switch (severity) {
+      case 'error':
+        color = 15400960;
+        break;
+      case 'warning':
+        color = 16744770;
+        break;
+
+      default:
+        color = 3742673;
+        break;
+    }
+
     const data = {
       embeds: [
         {
-          title: `[${severity}]: ${error.message}`,
-          description: error.stack,
+          title: [severity, moduleName, title].filter(Boolean).join(' - '),
+          color,
+          description: error?.stack,
           fields: Object.entries({ ...extra }).map(([key, value]) => ({
             name: key,
             value: JSON.stringify(value),
